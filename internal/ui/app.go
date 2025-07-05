1448062d5ad403cfc27fabc073987f1534ba51b1
Kyosuke Fujimoto <kyoro.f@gmail.com>
2025-07-05 09:46:27 +0900

    List styles

--- internal/ui/app.go
+++ internal/ui/app.go
@@ -1,25 +1,52 @@
 package ui
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/lusingander/gotip/internal/tip"
+)
+
+var (
+	selectedColor     = lipgloss.Color("#00ADD8")
+	listSelectedColor = lipgloss.Color("#5DC9E2")
+	cursorColor       = lipgloss.Color("#00ADD8")
+	borderColor       = lipgloss.Color("240")
 )
 
 type model struct {
 	list list.Model
+	w, h int
 
 	target *tip.Target
 }
 
 func newModel(items []list.Item) model {
-	list := list.New(items, list.NewDefaultDelegate(), 0, 0)
-	list.Title = "GOTIP"
+	itemDelegate := list.NewDefaultDelegate()
+	itemDelegate.Styles.SelectedTitle = itemDelegate.Styles.SelectedTitle.Foreground(listSelectedColor).BorderForeground(listSelectedColor)
+	itemDelegate.Styles.SelectedDesc = itemDelegate.Styles.SelectedDesc.Foreground(listSelectedColor).BorderForeground(listSelectedColor)
+	list := list.New(items, itemDelegate, 0, 0)
+	list.SetShowTitle(false)
+	list.SetShowFilter(false)
+	list.SetShowStatusBar(false)
+	list.SetShowHelp(false)
+	list.SetShowPagination(false)
+	list.FilterInput.Prompt = "Filtering: "
+	list.FilterInput.PromptStyle = lipgloss.NewStyle()
+	list.FilterInput.Cursor.Style = lipgloss.NewStyle().Foreground(cursorColor)
 
 	return model{
 		list:   list,
 		target: nil,
 	}
+}
+
+func (m *model) setSize(w, h int) {
+	m.w, m.h = w, h
+	m.list.SetSize(w, h-5)
 }
 
 var _ tea.Model = (*model)(nil)
@@ -33,7 +60,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.list.SetSize(msg.Width, msg.Height)
+		m.setSize(msg.Width, msg.Height)
 	case tea.KeyMsg:
 		if m.list.FilterState() == list.Filtering {
 			break
@@ -58,7 +85,53 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return m.list.View()
+	if m.w == 0 || m.h == 0 {
+		return ""
+	}
+
+	var headerContent string
+	if m.list.SelectedItem() != nil {
+		selected := m.list.SelectedItem().(*testCaseItem)
+		name := lipgloss.NewStyle().Foreground(selectedColor).Bold(true).Render(selected.name)
+		path := lipgloss.NewStyle().Foreground(selectedColor).Render(selected.path)
+		headerContent = name + "\n" + path
+	} else {
+		headerContent = "\n"
+	}
+
+	header := lipgloss.NewStyle().
+		Padding(0, 2).
+		Width(m.w).
+		Border(lipgloss.NormalBorder(), false, false, true, false).
+		BorderForeground(borderColor).
+		Render(headerContent)
+
+	var footerStatus string
+	switch m.list.FilterState() {
+	case list.Filtering:
+		footerStatus = strings.TrimRight(m.list.FilterInput.View(), " ")
+	case list.FilterApplied:
+		footerStatus = lipgloss.NewStyle().
+			Render(fmt.Sprintf("Filtered: %d items [Query: %s]", len(m.list.VisibleItems()), m.list.FilterValue()))
+	}
+
+	var footerSelected string
+	if len(m.list.VisibleItems()) > 0 {
+		footerSelected = lipgloss.NewStyle().
+			Render(fmt.Sprintf("%d / %d", m.list.Index()+1, len(m.list.VisibleItems())))
+	}
+
+	footerSpaceWidth := m.w - lipgloss.Width(footerStatus) - lipgloss.Width(footerSelected) - 2 /* padding */
+	footerSpace := strings.Repeat(" ", footerSpaceWidth)
+
+	footer := lipgloss.NewStyle().
+		Padding(0, 1).
+		Width(m.w).
+		Border(lipgloss.NormalBorder(), true, false, false, false).
+		BorderForeground(borderColor).
+		Render(footerStatus + footerSpace + footerSelected)
+
+	return lipgloss.JoinVertical(lipgloss.Left, header, m.list.View(), footer)
 }
 
 func Start(tests map[string][]*tip.TestFunction) (*tip.Target, error) {
