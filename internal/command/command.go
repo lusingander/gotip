9e642b2ddf0b7477083f194b75457a687a8b6df2
Kyosuke Fujimoto <kyoro.f@gmail.com>
2025-07-05 10:01:33 +0900

    Extract functions

--- internal/command/command.go
+++ internal/command/command.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/lusingander/gotip/internal/tip"
@@ -18,8 +16,8 @@ func Test(target *tip.Target, extraArgs []string) error {
 		return nil
 	}
 
-	packageName := relativePathToPackageName(target.Path)
-	testNameRegex := testNameToTestRunRegex(target.Name, target.IsUnresolved)
+	packageName := target.RelativePathToPackageName()
+	testNameRegex := target.TestNameToTestRunRegex()
 
 	args := []string{"test", "-run", testNameRegex, packageName}
 	cmd := exec.Command("go", append(args, extraArgs...)...)
@@ -29,20 +27,4 @@ func Test(target *tip.Target, extraArgs []string) error {
 
 	fmt.Println(outputStyle.Render(cmd.String()))
 	return cmd.Run()
-}
-
-func relativePathToPackageName(path string) string {
-	name := filepath.Dir(path)
-	name = filepath.ToSlash(name)
-	if !strings.HasPrefix(name, "./") {
-		name = "./" + name
-	}
-	return name
-}
-
-func testNameToTestRunRegex(name string, isUnresolved bool) string {
-	if isUnresolved {
-		return fmt.Sprintf("^%s", strings.TrimSuffix(name, tip.UnresolvedTestCaseName))
-	}
-	return fmt.Sprintf("^%s$", name)
 }
--- internal/tip/model.go
+++ internal/tip/model.go
@@ -1,5 +1,11 @@
 package tip
 
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
 type TestFunction struct {
 	Name string
 	Subs []*SubTest
@@ -15,4 +21,20 @@ type Target struct {
 	Path         string
 	Name         string
 	IsUnresolved bool
+}
+
+func (t *Target) RelativePathToPackageName() string {
+	name := filepath.Dir(t.Path)
+	name = filepath.ToSlash(name)
+	if !strings.HasPrefix(name, "./") {
+		name = "./" + name
+	}
+	return name
+}
+
+func (t *Target) TestNameToTestRunRegex() string {
+	if t.IsUnresolved {
+		return fmt.Sprintf("^%s", strings.TrimSuffix(t.Name, UnresolvedTestCaseName))
+	}
+	return fmt.Sprintf("^%s$", t.Name)
 }
