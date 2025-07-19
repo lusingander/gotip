dae0bfbe5bf582bb14f2a283b0ccd7691974d30a
Kyosuke Fujimoto <kyoro.f@gmail.com>
2025-07-19 10:57:18 +0900

    Define History and related functions

---
+++ internal/tip/history.go
@@ -0,0 +1,81 @@
+package tip
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Histories []*History
+
+type History struct {
+	Path            string
+	PackageName     string
+	TestNamePattern string
+	IsPrefix        bool
+	RunAt           time.Time
+}
+
+func LoadHistories(projectDir string) (*Histories, error) {
+	filePath, err := projectHistoriesFilePath(projectDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := os.Stat(filePath); err != nil {
+		return &Histories{}, nil
+	}
+
+	bytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var histories Histories
+	if err = json.Unmarshal(bytes, &histories); err != nil {
+		return nil, err
+	}
+	return &histories, nil
+}
+
+func SaveHistories(projectDir string, histories *Histories) error {
+	filePath, err := projectHistoriesFilePath(projectDir)
+	if err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(filepath.Dir(filePath), 0o700); err != nil {
+		return err
+	}
+
+	bytes, err := json.MarshalIndent(histories, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filePath, bytes, 0o600)
+}
+
+func projectHistoriesFilePath(projectDir string) (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	fileName, err := projectHistoriesFileName(projectDir)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ".local", "state", "gotip", "history", fileName), nil
+}
+
+func projectHistoriesFileName(projectDir string) (string, error) {
+	absDir, err := filepath.Abs(projectDir)
+	if err != nil {
+		return "", err
+	}
+	dir := filepath.ToSlash(absDir)
+	hash := md5.Sum([]byte(dir))
+	return hex.EncodeToString(hash[:]) + ".json", nil
+}
